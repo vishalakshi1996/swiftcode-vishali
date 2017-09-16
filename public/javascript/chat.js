a508be8652bc4a3703141904cf1132ca865c6d8c
vishali <vishalakshi027@gmail.com>
2017-09-16 11:48:45 +0000

    cpmeleted push

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -3,7 +3,7 @@ var app = angular.module('chatApp', ['ngMaterial']);
 app.controller('chatController', function ($scope, $sce){
 
 	$scope.messages = [];
-	var  exampleSocket =  new  WebSocket("ws://localhost:9000/chatSocket");
+	var  exampleSocket =  new  WebSocket("wss://swiftcode-vish.herokuapp.com/chatSocket");
     exampleSocket.onmessage  =   function  (event) {
         var jsonData = JSON.parse(event.data);
         jsonData.time = new Date().toLocaleTimeString();
